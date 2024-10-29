bae4b19b3c5a207cf5873e3b0776aa0108cf5266
BlendGashi <94861491+BlendGashi@users.noreply.github.com>
2024-10-29 03:43:27 +0100

    Update Slides.js

--- src/data/Slides.js
+++ src/data/Slides.js
@@ -1,17 +1,20 @@
-export const slides = [
+const slides = [
     {
-        id:1,
-        src: "https://iqq6kf0xmf.gjirafa.net/images/192fe3c1-ae80-4ab3-9d1d-7d2680771c2f/192fe3c1-ae80-4ab3-9d1d-7d2680771c2f.jpeg"
+        id: 1,
+        src: "https://iqq6kf0xmf.gjirafa.net/images/192fe3c1-ae80-4ab3-9d1d-7d2680771c2f/192fe3c1-ae80-4ab3-9d1d-7d2680771c2f.jpeg",
         alt:""
     },
     {
-        id:2,
-        src: "https://https://iqq6kf0xmf.gjirafa.net/images/1709a09a-a94c-4082-9381-a38ad595c5d4/1709a09a-a94c-4082-9381-a38ad595c5d4.jpeg.gjirafa.net/images/192fe3c1-ae80-4ab3-9d1d-7d2680771c2f/192fe3c1-ae80-4ab3-9d1d-7d2680771c2f.jpeg"
+        id: 2,
+        src: "https://iqq6kf0xmf.gjirafa.net/images/1709a09a-a94c-4082-9381-a38ad595c5d4/1709a09a-a94c-4082-9381-a38ad595c5d4.jpeg",
         alt:""
     },
     {
-        id:3,
-        src: "https://iqq6kf0xmf.gjirafa.net/images/192fe3c1-ae80-4ab3-9https://iqq6kf0xmf.gjirafa.net/images/a1e0ea09-e3f7-43cf-bba7-ab5b553b1caa/a1e0ea09-e3f7-43cf-bba7-ab5b553b1caa.jpegd1d-7d2680771c2f/192fe3c1-ae80-4ab3-9d1d-7d2680771c2f.jpeg"
+        id: 3,
+        src: "https://iqq6kf0xmf.gjirafa.net/images/a1e0ea09-e3f7-43cf-bba7-ab5b553b1caa/a1e0ea09-e3f7-43cf-bba7-ab5b553b1caa.jpeg",
         alt:""
     }
 ]
+
+export default slides;
+
