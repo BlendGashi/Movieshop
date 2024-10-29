6e9a8d5c0faf34963dfdcc7ba16eaef34be902e2
BlendGashi <94861491+BlendGashi@users.noreply.github.com>
2024-10-29 03:43:24 +0100

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Slider from '../components/Slider';
+import LatestMovies from '../components/LatestMovies';
 
 function Home() {
   return (
-    <></>
+    <>
+    <Slider/> 
+    <LatestMovies/>
+    </>
   )
 }
 
