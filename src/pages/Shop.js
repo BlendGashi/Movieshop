aab1cc68feabcfe9f982cc38b207374f8bd4de9a
BlendGashi <94861491+BlendGashi@users.noreply.github.com>
2024-10-30 05:24:09 +0100

    Update Shop.js

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,24 +1,41 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Container } from 'react-bootstrap';
-import Movie from "../components/Movie"
+import { Button, Container } from 'react-bootstrap';
+import Movie from "../components/Movie";
 import { Link } from 'react-router-dom';
 
 function Shop() {
-        const [page, setPage] =useState(1)
-        const [movies, setMovies] =useState()
-        const [totalPages, setTotalPages] =useState()
-
-
-useEffect(()  => {
-    axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=3e52e2f5350ae60de5e2fc58e818d2a0&page${page}`)
+        const [page, setPage] = useState(1)
+        const [movies, setMovies] = useState()
+        const [totalPages, setTotalPages] = useState()
+        const [query, setQuery] = useState()
+        
+useEffect(() => {
+    axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=3e52e2f5350ae60de5e2fc58e818d2a0&page=${page}`)
     .then(resp => {
         if(resp.status === 200) {
           setMovies(resp.data.results)
-          setTotalPages(resp.data.total_pages)}
+          setTotalPages(resp.data.total_pages)
+        }
     })
         .catch(error => console.log(error))
         }, [page])
+
+        const handleSearch = e => {
+          setQuery(e.target.value)
+          switch(e.keyCode) {
+            case 13:
+              axios.get(`https://api.themoviedb.org/3/search/movie?api_key=3e52e2f5350ae60de5e2fc58e818d2a0&query=${query}`)
+              .then(resp => {
+                  if(resp.status === 200) {
+                    setMovies(resp.data.results)
+                    setTotalPages(resp.data.total_pages)
+                  }
+              })
+                  .catch(error => console.log(error))
+            break
+          }
+        }
 
 const handlePrevPage = e => {
   e.preventDefault()
@@ -36,14 +53,23 @@ const handleNextPage = e => {
   }
 }
 
+
   return (
     <section className="py-5">
         <Container>
           <div className='d-flex justify-content-between align-items-center bg-light p-4 mb-5'>
-            <input type="search" className="form-control w-50" placeholder='Search movies ...'/>
+            <input type="search" className="form-control w-50" placeholder='Search movies ...' onKeyUp={handleSearch}/>
             <div>
-              <a href='#' onClick={handlePrevPage} class="btn btn-sm btn-outline-primary me-3">Prev</a>
-              <a href='#' onClick={handleNextPage} class="btn btn-sm btn-outline-primary">Next</a>
+              {
+                (query && query.length > 2) ? <>
+                  <Link to="/shop" reloadDocument variant="outline-primary">Back</Link>
+                </> : <>
+                  <a href='#' onClick={handlePrevPage} className="btn btn-sm btn-outline-primary">Prev</a>
+                  <span className='px-3'>{page}</span>
+                  <a href='#' onClick={handleNextPage} className="btn btn-sm btn-outline-primary">Next</a>
+                </>
+              }
+              
             </div>
           </div>
             <div className="row">
