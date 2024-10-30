81e8f9d52febbabac9adec1ed8ff5821c7764f9b
BlendGashi <94861491+BlendGashi@users.noreply.github.com>
2024-10-30 05:24:16 +0100

    Update Movie.js

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router-dom';
 
-function Movie() {
+function Movie({id, original_title, backdrop_path, popularity, vote_average}) {
+  const src = (backdrop_path !== null) ? `http://image.tmdb.org/t/p/w500/${backdrop_path}` : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSM4sEG5g9GFcy4SUxbzWNzUTf1jMISTDZrTw&s"
   return (
-    <></>
+    <Card>
+      <Card.Img variant="top" style={{height:"171px"}} src={src} />
+      <Card.Body>
+        <Card.Title style={{height: "50px"}}>{original_title}</Card.Title>
+        <div className='d-flex justify-content-between align-items-center' >
+        <Card.Text>
+          Popularity: {popularity}
+          <br/>
+          Vote avg: {vote_average}
+        </Card.Text>
+        <Link to={`/movie/${id}`} className="btn btn-outline-primary">&rarr;</Link>
+        </div>
+      </Card.Body>
+    </Card>
   )
 }
 
