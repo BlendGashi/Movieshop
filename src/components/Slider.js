645ead743ac19c8f0e617511cef2b5404b695026
BlendGashi <94861491+BlendGashi@users.noreply.github.com>
2024-10-30 05:24:13 +0100

    Update Slider.js

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Slides from "../data/Slides";
+import Carousel from 'react-bootstrap/Carousel';
+import Image from 'react-bootstrap/Image';
+
 
 function slider() {
   return (
-    <></>
+    <Carousel> 
+      { Slides && Slides.map(slide => <Carousel.Item key={slide.id}><Image src={slide.src} alt={slide.alt} /></Carousel.Item>) }
+      </Carousel>
   )
 }
 
