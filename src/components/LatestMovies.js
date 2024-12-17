12f3d3cd21be16beda0446db043393f2097a3139
BlendGashi <94861491+BlendGashi@users.noreply.github.com>
2024-12-17 18:05:30 +0100

    Update LatestMovies.js

--- src/components/LatestMovies.js
+++ src/components/LatestMovies.js
@@ -7,7 +7,6 @@ import { Link } from 'react-router-dom';
 function LatestMovies() {
         const [movies, setMovies] =useState()
 
-
 useEffect(()  => {
     axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=3e52e2f5350ae60de5e2fc58e818d2a0`)
     .then(resp => {
